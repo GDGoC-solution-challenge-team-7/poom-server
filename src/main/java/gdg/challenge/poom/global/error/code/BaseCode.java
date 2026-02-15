ae4a46dd0259565c82f4a7d95d763f25d357294a
yoojinche <chesunny917@gmail.com>
2026-02-15 13:38:28 +0900

    ✨ feat: 응답 통일 및 에러 핸들링 구현

---
+++ src/main/java/gdg/challenge/poom/global/error/code/BaseCode.java
@@ -0,0 +1,8 @@
+package gdg.challenge.poom.global.error.code;
+
+public interface BaseCode {
+
+    ReasonDTO getReason();
+
+    ReasonDTO getReasonHttpStatus();
+}
---
+++ src/main/java/gdg/challenge/poom/global/error/code/BaseErrorCode.java
@@ -0,0 +1,7 @@
+package gdg.challenge.poom.global.error.code;
+
+public interface BaseErrorCode {
+
+    ErrorReasonDTO getReason();
+    ErrorReasonDTO getReasonHttpStatus();
+}
