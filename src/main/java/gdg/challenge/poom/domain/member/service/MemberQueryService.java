b9e8265dc7ca538ae278ac44ac98a9cff443ea08
yoojinche <chesunny917@gmail.com>
2026-02-24 00:03:51 +0900

    ✨ feat: ErrorCode 수정

--- src/main/java/gdg/challenge/poom/domain/member/service/MemberQueryService.java
+++ src/main/java/gdg/challenge/poom/domain/member/service/MemberQueryService.java
@@ -2,6 +2,8 @@ package gdg.challenge.poom.domain.member.service;
 
 import gdg.challenge.poom.domain.member.entity.Member;
 import gdg.challenge.poom.domain.member.repository.MemberRepository;
+import gdg.challenge.poom.global.error.code.status.MemberErrorCode;
+import gdg.challenge.poom.global.error.exception.handler.MemberException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +17,6 @@ public class MemberQueryService {
 
     public Member findById(Long memberId){
         return memberRepository.findById(memberId)
-                .orElseThrow(() -> new IllegalArgumentException("TODO"));
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
     }
 }
