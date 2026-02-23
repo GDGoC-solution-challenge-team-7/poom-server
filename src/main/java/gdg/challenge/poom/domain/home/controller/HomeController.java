dcc4c0f59c084cc1a009d385f3c413e5efc41b20
yoojinche <chesunny917@gmail.com>
2026-02-23 12:37:11 +0900

    ✨ feat: 홈 화면 출산 디데이 조회 구현

--- src/main/java/gdg/challenge/poom/domain/home/controller/HomeController.java
+++ src/main/java/gdg/challenge/poom/domain/home/controller/HomeController.java
@@ -1,10 +1,15 @@
 package gdg.challenge.poom.domain.home.controller;
 
 import gdg.challenge.poom.domain.home.dto.HomeResponseDTO;
+import gdg.challenge.poom.domain.home.service.HomeService;
+import gdg.challenge.poom.domain.member.entity.enums.UserType;
 import gdg.challenge.poom.global.error.ApiResponse;
+import gdg.challenge.poom.global.security.domain.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,9 +18,14 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "홈 화면 API")
 public class HomeController {
 
+    private final HomeService homeService;
+
     @Operation(summary = "출산 디데이 조회 API", description = "홈 화면에서 출산 디데이 조회하는 API")
     @GetMapping("/d-day")
-    public ApiResponse<HomeResponseDTO.MemberBirthDate> getBirthDueDate(@RequestParam("userType") String userType){
-        return ApiResponse.onSuccess(null);
+    public ApiResponse<HomeResponseDTO.MemberBirthDate> getBirthDueDate(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ){
+        HomeResponseDTO.MemberBirthDate birthDueDate = homeService.getBirthDueDate(customUserDetails.getMemberId());
+        return ApiResponse.onSuccess(birthDueDate);
     }
 }
--- src/main/java/gdg/challenge/poom/domain/home/service/HomeService.java
+++ src/main/java/gdg/challenge/poom/domain/home/service/HomeService.java
@@ -1,30 +1,56 @@
 package gdg.challenge.poom.domain.home.service;
 
+import gdg.challenge.poom.domain.home.converter.HomeConverter;
+import gdg.challenge.poom.domain.home.dto.DateType;
+import gdg.challenge.poom.domain.home.dto.HomeResponseDTO;
 import gdg.challenge.poom.domain.member.entity.Member;
 import gdg.challenge.poom.domain.member.entity.enums.UserType;
 import gdg.challenge.poom.domain.member.repository.MemberRepository;
 import gdg.challenge.poom.global.error.code.status.MemberErrorCode;
 import gdg.challenge.poom.global.error.exception.handler.MemberException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
+@Service
 public class HomeService {
 
     private final MemberRepository memberRepository;
 
-    public void getBirthDueDate(UserType userType, Long memberId){
+    public HomeResponseDTO.MemberBirthDate getBirthDueDate(Long memberId){
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+        UserType userType = member.getUserType();
+        LocalDate today = LocalDate.now();
+        long between;
+
         if (userType == UserType.PREGNANT){
-//            member.getChildBirthDueDate() - LocalDate.now();
+            LocalDate childBirthDueDate = member.getChildBirthDueDate();
+            between = ChronoUnit.DAYS.between(today, childBirthDueDate);
         } else if (userType == UserType.POSTPARTUM){
-
+            LocalDate childBirthDate = member.getChildBirthDate();
+            between = ChronoUnit.DAYS.between(today, childBirthDate);
         } else {
-
+            return HomeConverter.toMemberBirthDate(userType, null, null);
         }
+        return calculateDateUnit(userType, between);
+    }
+
+    private HomeResponseDTO.MemberBirthDate calculateDateUnit(UserType userType, long between){
+        // DateType.DAY
+        if (between <= 30) {
+            return HomeConverter.toMemberBirthDate(userType, DateType.DAY, (int) between);
+        }
+        // DateType.MONTH
+        if (between <= 30 * 12) {
+            return HomeConverter.toMemberBirthDate(userType, DateType.MONTH, (int) (between / 30));
+        }
+        // DateType.YEAR
+        return HomeConverter.toMemberBirthDate(userType, DateType.YEAR, (int) (between / (30 * 12)));
     }
 }
