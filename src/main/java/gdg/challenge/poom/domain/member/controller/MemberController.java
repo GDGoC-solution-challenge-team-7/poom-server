4e52fb204330feafb11422d216ad380e054997a8
yoojinche <chesunny917@gmail.com>
2026-02-24 15:11:08 +0900

    ✨ feat: 회원 swagger 명명 변경

--- src/main/java/gdg/challenge/poom/domain/member/controller/MemberController.java
+++ src/main/java/gdg/challenge/poom/domain/member/controller/MemberController.java
@@ -7,6 +7,7 @@ import gdg.challenge.poom.domain.member.service.MemberQueryService;
 import gdg.challenge.poom.global.error.ApiResponse;
 import gdg.challenge.poom.global.security.domain.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "회원 API")
 public class MemberController {
 
     private final MemberCommandService memberCommandService;
