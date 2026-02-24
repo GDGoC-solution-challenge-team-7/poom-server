f53e363b8559670f9941cdf50baa910a50bffc73
yoojinche <chesunny917@gmail.com>
2026-02-24 13:41:04 +0900

    ✨ feat: application.yml 파일에서 상수가져오는 에러 수정

--- src/main/java/gdg/challenge/poom/global/data/GenAiConfigData.java
+++ src/main/java/gdg/challenge/poom/global/data/GenAiConfigData.java
@@ -1,10 +1,12 @@
 package gdg.challenge.poom.global.data;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "spring.ai.google.genai")
 public class GenAiConfigData {
