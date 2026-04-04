096ce9b10a2caaaec5ee87b0d5c32883046c5478
yoojinche <chesunny917@gmail.com>
2026-04-05 05:39:20 +0900

    🐛 fix: OAuth Jwtfilter에러 해결

--- src/main/java/gdg/challenge/poom/global/security/filter/JwtFilter.java
+++ src/main/java/gdg/challenge/poom/global/security/filter/JwtFilter.java
@@ -30,8 +30,14 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         log.info("[JWT] {} {}", request.getMethod(), request.getRequestURI());
         String token = resolveToken(request);
+
+        if (!StringUtils.hasText(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         Long memberId = jwtUtil.getMemberId(token);
-        if (!StringUtils.hasText(token) | memberId == null) {
+        if (memberId == null) {
             filterChain.doFilter(request, response);
             return;
         }
