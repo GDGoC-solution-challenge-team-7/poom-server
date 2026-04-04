7dfc5687d0fabf2ae2992e8d2a4359f2bd41f806
yoojinche <chesunny917@gmail.com>
2026-04-05 00:44:49 +0900

    ♻️ refactor: 알림 디버깅용 코드 추가

---
+++ src/main/java/gdg/challenge/poom/global/error/code/status/AlarmErrorCode.java
@@ -0,0 +1,37 @@
+package gdg.challenge.poom.global.error.code.status;
+
+import gdg.challenge.poom.global.error.code.BaseErrorCode;
+import gdg.challenge.poom.global.error.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum AlarmErrorCode implements BaseErrorCode {
+    FCM_SEND_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "FCM_500_1", "알림 전송에 실패했습니다.")
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ErrorReasonDTO getReason() {
+        return gdg.challenge.poom.global.error.code.ErrorReasonDTO.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(false)
+                .build();
+    }
+
+    @Override
+    public ErrorReasonDTO getReasonHttpStatus() {
+        return gdg.challenge.poom.global.error.code.ErrorReasonDTO.builder()
+                .httpStatus(status)
+                .message(message)
+                .code(code)
+                .isSuccess(false)
+                .build();
+    }
+}
