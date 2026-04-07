111895898ccebc9529d4846329526093eece237f
yoojinche <chesunny917@gmail.com>
2026-04-07 19:02:10 +0900

    🐛 fix: saveAll 메소드 삭제

--- src/main/java/gdg/challenge/poom/domain/chat/repository/ChatMessageImageRepository.java
+++ src/main/java/gdg/challenge/poom/domain/chat/repository/ChatMessageImageRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ChatMessageImageRepository extends JpaRepository<ChatMessageImage, Long> {
-    List<ChatMessageImage> saveAll(List<ChatMessageImage> chatMessageImages);
 }
