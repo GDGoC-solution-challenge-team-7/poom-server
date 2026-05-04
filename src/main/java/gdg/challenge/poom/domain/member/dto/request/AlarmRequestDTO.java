23ebf62e302c51406b9eea33fc3991f5a567a66b
yoojinche <chesunny917@gmail.com>
2026-05-04 13:13:46 +0900

    ♻️ refactor: 시간 형식 포맷팅

--- src/main/java/gdg/challenge/poom/domain/member/dto/request/AlarmRequestDTO.java
+++ src/main/java/gdg/challenge/poom/domain/member/dto/request/AlarmRequestDTO.java
@@ -3,6 +3,7 @@ package gdg.challenge.poom.domain.member.dto.request;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import gdg.challenge.poom.domain.chat.entity.enums.CharacterType;
 import gdg.challenge.poom.domain.member.entity.enums.AlarmType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -28,6 +29,11 @@ public record AlarmRequestDTO (){
 
     public record UpdateAlarmSetting(
             Boolean pushAlarm,
+            @Schema(
+                    type = "string",
+                    example = "12:34:00",
+                    description = "매일 알림 시간"
+            )
             @JsonFormat(pattern = "HH:mm:ss")
             LocalTime dailyAlarmTime
     ){}
