b7a1c81cb7d985b8ea1b02a900333916014c2ea2
yoojinche <chesunny917@gmail.com>
2026-06-06 16:03:03 +0900

    ♻️ refactor: GCS 로컬 접근 허용

--- src/main/java/gdg/challenge/poom/domain/chat/entity/enums/UploadDomain.java
+++ src/main/java/gdg/challenge/poom/domain/chat/entity/enums/UploadDomain.java
@@ -1,5 +1,5 @@
 package gdg.challenge.poom.domain.chat.entity.enums;
 
 public enum UploadDomain {
-    PROFILE_IMAGE, CHAT_IMAGE, EXPERT_VERIFICATION, JOURNAL
+    PROFILE_IMAGE, CHAT_IMAGE, EXPERT_VERIFICATION, JOURNAL_IMAGE
 }
